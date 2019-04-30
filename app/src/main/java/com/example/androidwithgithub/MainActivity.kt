f2dfc14cf2c660ffdc181d863723d4c941c45f3a
mrdanso96 <benmartindanso>
2019-04-30 10:58:18 +0200

    update two files one in the layout and the second in the kotlin

--- app/src/main/java/com/example/androidwithgithub/MainActivity.kt
+++ app/src/main/java/com/example/androidwithgithub/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.androidwithgithub
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,6 +13,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         //Our first Project on Github
-        
+
+        val text = findViewById<TextView>(R.id.text_view)
+        val btn = findViewById<Button>(R.id.btn)
+        val image = findViewById<ImageView>(R.id.image)
+
+
+
     }
 }
